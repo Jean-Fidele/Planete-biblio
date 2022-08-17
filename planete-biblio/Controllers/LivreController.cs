31b2973b4342de03e842d70dcd90e07185b6fc82
Fidele <fidele999@gmail.com>
2022-08-17 19:44:03 +0200

    Ajout de create file pour le livre

--- planete-biblio/Controllers/LivreController.cs
+++ planete-biblio/Controllers/LivreController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using planete_biblio.Data;
+using planete_biblio.Entities;
 
 namespace planete_biblio.Controllers
 {
@@ -16,6 +17,31 @@ namespace planete_biblio.Controllers
         public IActionResult Index()
         {
             return View(ctx.Livre.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View(new Livre());
+        }
+
+        [HttpPost]
+        public ActionResult Create(Livre livre)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(livre);
+            }
+
+            try
+            {
+                ctx.Livre.Add(livre);
+                ctx.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(livre);
+            }
         }
     }
 }
