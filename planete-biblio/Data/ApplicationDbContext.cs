25130c12c7fac1fd55d76a1270a6bd1f8726e2c9
Rakotomamonjy Jean Fidele <fidele.r@o3.mg>
2022-10-03 08:46:59 +0300

    - Add console.log for Entity framework core

--- planete-biblio/Data/ApplicationDbContext.cs
+++ planete-biblio/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using planete_biblio.Entities;
 using planete_biblio.Models;
+using System.Reflection.Emit;
 
 namespace planete_biblio.Data
 {
@@ -13,12 +14,16 @@ namespace planete_biblio.Data
         {
         }
 
+        
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Seed();
+        }
+
         public DbSet<Livre> Livre { get; set; }
-
         public DbSet<Editeur> Editeur { get; set; }
-
         public DbSet<Auteur> Auteur { get; set; }
-
         public DbSet<Categorie> Categorie { get; set; }
     }
 }
--- planete-biblio/Program.cs
+++ planete-biblio/Program.cs
@@ -10,45 +10,33 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-
+    options.UseSqlServer(connectionString)
+           .LogTo(Console.WriteLine, LogLevel.Information)
+);
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-
-
+                .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.ConfigureApplicationCookie(options =>
 {
-    // Cookie settings
-    //options.Cookie.HttpOnly = true;
     options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
-    
     options.LoginPath = "/Account/Login";
     options.AccessDeniedPath = "/Account/AccessDenied";
     options.SlidingExpiration = true;
 });
 
 var app = builder.Build();
-
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
 app.Run();
